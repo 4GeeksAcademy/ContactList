8d16533fb06ab3cd54d110b8a7bd26d8076ef932
María Pérez <123765659+mariaperez15@users.noreply.github.com>
2023-08-31 16:59:09 +0000

    corrección de errores

--- src/js/views/register.js
+++ src/js/views/register.js
@@ -3,45 +3,45 @@ import React from "react";
 export const Register = () => {
 
     const handleChange = (event) => {
-        console.log(event.target)
+        console.log(event.target.value)
     }
 
     return(
-        <div classNameName="container">
-            <div classNameName="row">            
-                <h3 classNameName="mb-3">Registro de Contactos</h3>
-                <div classNameName="col-12">
-                    <div classNameName="mb-3">
-                        <label for="name" classNameName="form-label">Full Name</label>
-                        <input type="text" classNameName="form-control" id="name" placeholder="Full Name" onChange={handleChange}/>
+        <div className="container">
+            <div className="row">            
+                <h3 className="mb-3">Registro de Contactos</h3>
+                <div className="col-12">
+                    <div className="mb-3">
+                        <label htmlFor="name" className="form-label">Full Name</label>
+                        <input type="text" className="form-control" id="name" placeholder="Full Name" onChange={handleChange}/>
                     </div>
                 </div>
-                <div classNameName="col-12">
+                <div className="col-12">
                     <div className="mb-3">
-                        <label for="email" className="form-label">Email</label>
+                        <label htmlFor="email" className="form-label">Email</label>
                         <input type="email" className="form-control" id="email" placeholder="Email"/>
                     </div>
                 </div>
-                <div classNameName="col-12">
+                <div className="col-12">
                     <div className="mb-3">
-                        <label for="address" className="form-label">Address</label>
+                        <label htmlFor="address" className="form-label">Address</label>
                         <input type="text" className="form-control" id="address" placeholder="Address"/>
                     </div>
                 </div>
-                <div classNameName="col-12">
+                <div className="col-12">
                     <div className="mb-3">
-                        <label for="phone" className="form-label">Phone</label>
+                        <label htmlFor="phone" className="form-label">Phone</label>
                         <input type="n" className="form-control" id="phone" placeholder="Phone Number"/>
                     </div>
                 </div>
-                <div classNameName="col-12">
+                <div className="col-12">
                     <div className="mb-3">
-                        <label for="agenda_slug" className="form-label">Agenda</label>
+                        <label htmlFor="agenda_slug" className="form-label">Agenda</label>
                         <input type="text" className="form-control" id="agenda_slug" placeholder="Maria's Agenda" disabled/>
                     </div>
                 </div>
-                <div classNameName="d-flex justify-content-center">
-                 <button classNameName="btn btn-info col-5">Registrar Contacto</button>
+                <div className="d-flex justify-content-center">
+                    <button className="btn btn-info col-5">Registrar Contacto</button>
                 </div>
             </div>
         </div>
